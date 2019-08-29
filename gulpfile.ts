eafc2d201edd52f4d534aba64a778ff199615e10
damon <damon@mahuika.co.nz>
2019-08-29 14:53:11 +1200

    Switch to gulp-typescript

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,17 +1,20 @@
 import ensureGitUpToDate from '@mindhive/deploy/ensureGitUpToDate'
-import execFile from '@mindhive/deploy/execFile'
 import yarnPublish from '@mindhive/deploy/yarnPublish'
 import del from 'del'
 import gulp from 'gulp'
+import ts from 'gulp-typescript'
 
 const distDir = 'dist'
+
+const tsProj = ts.createProject('src/tsconfig.json')
 
 const clean = () => del(distDir)
 
 const build = () =>
-  execFile('tsc', ['--outDir', distDir, '--project', 'src'], {
-    pipeOutput: true,
-  })
+  tsProj
+    .src()
+    .pipe(tsProj())
+    .pipe(gulp.dest(distDir))
 
 export const dist = gulp.series(clean, build)
 
